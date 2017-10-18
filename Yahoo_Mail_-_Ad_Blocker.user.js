1f9492ab25dd305b4985083fabcb82aedca93cff
Glen <tgp1994@users.noreply.github.com>
2017-10-18 12:15:04 -0700

    Version 1.8 - updated for new YMail UI

--- Yahoo_Mail_-_Ad_Blocker.user.js
+++ Yahoo_Mail_-_Ad_Blocker.user.js
@@ -2,8 +2,9 @@
 // @name        Yahoo Mail - Ad Blocker
 // @namespace   ymail_ads
 // @description YMail Ads Remover. Hides all the Ad panels (Top, Left, Right) in the NEW Yahoo Mail and expands them to use the full screen, to make your Yahoo Mail completely equivalent to the Paid version for free!
+// @include     *://*.mail.yahoo.com/*
 // @include     *://mail.yahoo.com/*
-// @version     1.3
+// @version     1.8
 // @author      Volkan K.
 // @license     Apache License, Version 2.0; http://www.apache.org/licenses/LICENSE-2.0
 // @grant       GM_addStyle
@@ -13,22 +14,52 @@
 
 this.$ = this.jQuery = jQuery.noConflict(true);
 
-if ( typeof unsafeWindow.NeoConfig.noDarla !== 'undefined' ) { unsafeWindow.NeoConfig.noDarla=1; }
-if ( typeof unsafeWindow.NeoConfig.hasAds !== 'undefined' ) { unsafeWindow.NeoConfig.hasAds=0; }
-$( document ).ready(function() {
+function AddStyle(cssText)
+{
+	//alert("addstyle call");// for debug
+	let styleEl = document.createElement("style");
+	styleEl.textContent = cssText;
+	styleEl.setAttribute("type", "text/css");
+	styleEl.setAttribute("id", "my_great_css");
+	let headEl = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
+	headEl.appendChild(styleEl);
+	//alert(headEl); // for debug
+}
+function rem_w_parent (remove_list) {
+	parent_suspects=jQuery(remove_list).parent();
+	jQuery(remove_list).remove();
+	parent_suspects.filter(":empty").remove();
+}
+function my_ad_remover() {
   //alert('test'); // for debugging.
-  $(".rightRailAdContainer, .mb, .left_mb, .ad-filler, .layoutfixer .c3").remove();
-  if ( typeof unsafeWindow.NeoConfig.noDarla !== 'undefined' ) { unsafeWindow.NeoConfig.noDarla=1; }
-  if ( typeof unsafeWindow.NeoConfig.hasAds !== 'undefined' ) { unsafeWindow.NeoConfig.hasAds=0; }
-});
-
-//, #spritzAd
-GM_addStyle("\
-#mbAds, #tgtTL1, #theAd, #sb_rel_tgtTL1, #masSearchAd, .sky-ad, #mbRecAds, .adchoices, .adlink, .mb-list-ad, .mb-rec-ad, #slot_TL1, #slot_mbrec, #slot_MB, #mobilizer, #yucs-disclaimer { display:none !important; width:0px !important; position: absolute !important; left: -999px !important; top: -999px !important; padding: 0px !important; z-index: -999 !important; } \
-#shellcontent {right: 0px !important;} \
+	rem_w_parent("#slot_LREC, #slot_REC, #slot_MON, #theMNWAd, div[data-test-id='right-rail-ad'], a[data-test-id='pencil-ad'], .rightRailAdContainer, .mb, .left_mb, .ad-filler, .layoutfixer .c3, #mbAds, #tgtTL1, #theAd, #sb_rel_tgtTL1, #masSearchAd, .sky-ad, #mbRecAds, .adchoices, .adlink, .mb-list-ad, .mb-rec-ad, #slot_TL1, #slot_mbrec, #slot_MB, #mobilizer, #yucs-disclaimer, #dd_search_results_container, .search-message-container-ad, #emptyFolderVideo > .wrapperpane, .empty-folder-submsg2");
+	if ( typeof unsafeWindow.NeoConfig !== 'undefined' ) {
+		if ( typeof unsafeWindow.NeoConfig.noDarla !== 'undefined' ) { unsafeWindow.NeoConfig.noDarla=1; }
+		if ( typeof unsafeWindow.NeoConfig.hasAds !== 'undefined' ) { unsafeWindow.NeoConfig.hasAds=0; }
+		if ( typeof unsafeWindow.NeoConfig.prefetchAd !== 'undefined' ) { unsafeWindow.NeoConfig.prefetchAd=0; }
+	}
+	if ($("#my_great_css").length<1){
+	//, #spritzAd
+	AddStyle("\
+#slot_LREC, #slot_REC, #slot_MON, #theMNWAd, div[data-test-id=\"right-rail-ad\"], a[data-test-id=\"pencil-ad\"], #mbAds, #tgtTL1, #theAd, #sb_rel_tgtTL1, #masSearchAd, .sky-ad, #mbRecAds, .adchoices, .adlink, .mb-list-ad, .mb-rec-ad, #slot_TL1, #slot_mbrec, #slot_MB, #mobilizer, #yucs-disclaimer, #dd_search_results_container, .search-message-container-ad, #emptyFolderVideo > .wrapperpane, .empty-folder-submsg2 { display:none !important; width:0px !important; position: absolute !important; left: -999px !important; top: -999px !important; padding: 0px !important; z-index: -999 !important; } \
+#shellcontent,#toolbar,.messagepane .right-ar {right: 0px !important;} \
 #main {max-width:none !important;} \
 #shellnavigation {bottom: inherit !important; } \
+#msg-list.with-right-rail .list-view-items { padding-right : 0px !important;} \
+#main, #yucs, #yuhead-bucket {max-width:none !important} \
+.fullpage #main, .fullpage #yucs, .fullpage #yuhead-bucket {margin-right:0 !important} \
+.mb-rec-ad {display:none !important; height:0 !important; border:none !important; overflow-x:hidden !important} \
+.mb-list-ad {display:none !important; height:0 !important; border:none !important; overflow-x:hidden !important} \
+div[data-test-id='comms-properties-bar'] {flex-direction: column !important;height: auto !important} \
+div[data-test-id='comms-properties'] {flex-direction: column !important;margin-top: 14px !important} \
+div[data-test-id='comms-properties'] > a {margin-right: 0 !important; margin-bottom: 14px !important} \
+span[data-test-id='settings-link-label'] {display:none !important} \
 ");
+	}
+}
+my_ad_remover();
+$( document ).ready(my_ad_remover);
+window.addEventListener("DOMNodeInserted", my_ad_remover, false);
 
 // for the old, 'Basic' Yahoo Mail. 
 var oldYahooMailAds = $(".with-ads");
@@ -38,5 +69,5 @@ if(oldYahooMailAds.length>0)
 	.mb > .tbl{ display:none !important; width:0px !important; position: absolute !important; left: -999px !important; top: -999px !important; padding: 0px !important; z-index: -999 !important; } \
 	");
 
-	oldYahooMailAds.removeClass("with-ads");
+	oldYahooMailAds.removeClass("with-ads").addClass("withoutad");
 }
